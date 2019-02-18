dca8132b1a5b75c0dfac94a9f773741e00f6fc22
Andrew Huth <andrew.huth@airbnb.com>
2019-02-17 21:26:57 -0800

    Chapter 3 - invariants

--- test/pbt_test.exs
+++ test/pbt_test.exs
@@ -18,4 +18,35 @@ defmodule PbtTest do
       known_last == List.last(known_list)
     end
   end
+
+  property "a sorted list has ordered pairs" do
+    forall list <- list(term()) do
+      is_ordered(Enum.sort(list))
+    end
+  end
+
+  def is_ordered([a, b | tail]) do
+    a <= b and is_ordered([b | tail])
+  end
+  def is_ordered(_), do: true
+
+  property "a sorted list keeps its size" do
+    forall list <- list(term()) do
+      length(list) === length(Enum.sort(list))
+    end
+  end
+
+  property "a sorted list has no added elements" do
+    forall list <- list(term()) do
+      sorted = Enum.sort(list)
+      Enum.all?(sorted, fn element -> element in list end)
+    end
+  end
+
+  property "a sorted list has no missing elements" do
+    forall list <- list(term()) do
+      sorted = Enum.sort(list)
+      Enum.all?(list, fn element -> element in sorted end)
+    end
+  end
 end
